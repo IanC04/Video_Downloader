07ac3c20062a4586812d7837ee70a7601931f875
Ian <44680390+IanC04@users.noreply.github.com>
2024-01-09 23:54:31 -0600

    Use GUI

--- YouTube Download.bat
+++ YouTube Download.bat
@@ -4,6 +4,10 @@ cls
 SET python_url=https://raw.githubusercontent.com/IanC04/Video_Downloader/master/YouTube.py
 SET requirements_url=https://raw.githubusercontent.com/IanC04/Video_Downloader/master/requirements.txt
 
+@REM REM Change directory to the script's directory
+cd /D "%~dp0"
+echo Current directory: %cd%
+
 IF NOT EXIST .\requirements.txt (
     echo requirements.txt not found!
     curl -o requirements.txt %requirements_url%
@@ -18,9 +22,10 @@ IF NOT EXIST .\venv\Scripts\activate.bat (
     echo Creating virtual environment...
     python -m venv venv
     echo Installing dependencies...
-    call .\venv\Scripts\activate.bat && pip install -r requirements.txt && python -m YouTube.py
-)
-ELSE (
+    call .\venv\Scripts\activate.bat && pip install -r requirements.txt && python -m YouTube
+) ELSE (
     echo Activating virtual environment...
-    call .\venv\Scripts\activate.bat && python -m YouTube.py
+    call .\venv\Scripts\activate.bat && python -m YouTube
 )
+
+PAUSE
