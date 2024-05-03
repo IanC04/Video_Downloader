eaeca2511388ccefef9fb4cb48d987f1d32f4840
Ian <44680390+IanC04@users.noreply.github.com>
2024-05-03 17:23:54 -0500

    Added support for audio and quality options. Improved appearance and added download icon. New features: Option to open file after downloading.

--- YouTube Download.bat
+++ YouTube Download.bat
@@ -1,12 +1,18 @@
 @echo off & title %~nx0
 cls
 
-SET python_url=https://raw.githubusercontent.com/IanC04/Video_Downloader/master/YouTube.py
+SET icon_url=https://raw.githubusercontent.com/IanC04/Video_Downloader/master/icon.ico
 SET requirements_url=https://raw.githubusercontent.com/IanC04/Video_Downloader/master/requirements.txt
+SET python_url=https://raw.githubusercontent.com/IanC04/Video_Downloader/master/YouTube.py
 
 @REM REM Change directory to the script's directory
 cd /D "%~dp0"
 echo Current directory: %cd%
+
+IF NOT EXIST .\icon.ico (
+    echo icon.ico not found!
+    curl -o icon.ico %icon_url%
+)
 
 IF NOT EXIST .\requirements.txt (
     echo requirements.txt not found!
